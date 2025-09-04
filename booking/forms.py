9f6b4ebc5d5876522bfd90adf896a475b155f208
dagmara-szproch <dagmamara@gmail.com>
2025-09-04 22:34:25 +0100

    Add booking form validation in view

--- booking/forms.py
+++ booking/forms.py
@@ -5,3 +5,9 @@ class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
         fields = ('booking_date', 'time_slot', 'number_of_people', 'special_requests',)
+
+        widgets = {
+            'booking_date': forms.TextInput(attrs={'class': 'flatpickr', 'placeholder': 'Select Date'}),
+            'number_of_people': forms.Select(choices=[(i, i) for i in range(1, 7)]), # Allow selection from 1 to 6 people
+            'special_requests': forms.Textarea(attrs={'rows': 3, 'placeholder': 'Any special requests?'}),
+        }
--- booking/views.py
+++ booking/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from django.db.models import Sum
 from .forms import BookingForm
 from .models import Booking
 from restaurant.models import Restaurant
@@ -23,16 +24,38 @@ def create_booking(request, slug):
             booking = form.save(commit=False)
             booking.user = request.user
             booking.restaurant = restaurant # Link booking to the selected restaurant
-            booking.save()
-            return redirect('my_bookings') # Redirect to a page showing user's bookings
+
+            # check unique booking constraint
+            if Booking.objects.filter(
+                user=request.user, 
+                restaurant=restaurant, 
+                booking_date=booking.booking_date, 
+                time_slot=booking.time_slot
+            ).exists():
+                form.add_error(None, "You already have a booking for this restaurant at the selected date and time.")
+            
+            # check restaurant capacity
+            current_bookings = Booking.objects.filter(
+                restaurant=restaurant,
+                booking_date=booking.booking_date,
+                time_slot=booking.time_slot,
+            ).aggregate(Sum('number_of_people'))['number_of_people__sum'] or 0
+
+            if current_bookings + booking.number_of_people > restaurant.online_capacity:
+                form.add_error('number_of_people', "The restaurant cannot accommodate your booking due to capacity limits. Please choose a different time or reduce the number of people.")
+
+            if not form.errors:
+                booking.save()
+                return redirect('my_bookings') # Redirect to a page showing user's bookings
+
     else:
         form = BookingForm()
-  
+        
     return render(
         request, 
         'booking/create_booking.html',
         {
-            'form': form, 
+            'form': form,
             'restaurant': restaurant
         }
     )
